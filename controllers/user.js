e84b9d97c66b34f58188d73f880278bf4d11c0b2
Milan Blaz <=>
2021-09-02 08:18:36 +0200

    added auth route as not everything was user related

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,5 @@
 const { User, RefreshToken } = require('../models');
-const {
-  generateAccessToken,
-  generateRefreshToken,
-  destroyToken,
-  jwtAuth,
-  verifyRefreshToken,
-} = require('../config/auth');
+
 const { uploadFile } = require('../config/googleDrive');
 require('dotenv').config();
 
@@ -39,16 +33,6 @@ exports.getUser = async (req, res) => {
   }
 };
 
-exports.getAuthUser = async (req, res) => {
-  try {
-    res.status(200);
-    res.json({ user: req.userData });
-  } catch (err) {
-    res.json({ error });
-    res.status(500);
-  }
-};
-
 exports.createNewUser = async (req, res) => {
   const { firstName, lastName, email, password, profilePicture } = req.body;
   try {
@@ -77,82 +61,6 @@ exports.deleteUser = async (req, res) => {
     res.json({ error });
     res.status(500);
   }
-};
-
-exports.login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const userData = await User.findOne({ where: { email } });
-
-    if (!userData) {
-      res.status(404);
-      res.json({ error: 'User not found.' });
-      return;
-    }
-
-    const isValid = await userData?.validPassword(password);
-
-    if (!isValid) {
-      res.status(403);
-      res.json({ error: 'Password is incorrect.' });
-      return;
-    }
-
-    const user = {
-      email: userData.email,
-      id: userData.id,
-      firstName: userData.firstName,
-      lastName: userData.lastName,
-    };
-
-    const accessToken = generateAccessToken(user);
-    const refreshToken = generateRefreshToken(user);
-
-    await RefreshToken.update(
-      { value: refreshToken },
-      { where: { userId: userData.id } }
-    );
-
-    res.json({ tokenType: 'Bearer', accessToken, refreshToken });
-  } catch (error) {
-    res.json({ error });
-    res.status(500);
-  }
-};
-
-exports.logout = async (req, res) => {
-  try {
-    const result = await RefreshToken.update(
-      { value: null },
-      { where: { userId: req.params.id } }
-    );
-
-    res.status(204).end();
-  } catch (error) {
-    res.json({ error });
-    res.status(500);
-  }
-};
-
-exports.renewToken = async (req, res) => {
-  const { refreshToken } = req.body;
-
-  const dbRefreshToken = await RefreshToken.findOne({
-    where: { value: refreshToken },
-  });
-
-  if (refreshToken == null) {
-    res.status(401).end();
-    return null;
-  }
-
-  if (dbRefreshToken?.value !== refreshToken) {
-    res.status(403).end();
-    return null;
-  }
-  const result = await verifyRefreshToken(res, refreshToken);
-  res.status(200);
-  res.json({ accessToken: result });
 };
 
 exports.uploadProfilePicture = async (req, res) => {
